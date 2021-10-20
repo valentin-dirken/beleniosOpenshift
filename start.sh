9fce84c970c1a12abe4b2c39098ce2c947e226c7
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2021-10-20 21:33:00 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "cd /home/belenios/install"
+cd /home/belenios/install
+pwd
+echo "eval $(opam env)"
+eval $(opam env) 
+echo "/home/belenios/install/demo/run-server.sh &"
+/home/belenios/install/demo/run-server.sh &
+
+echo 'Server launching...'
+
+sleep 5
+
+echo 'Server testing ...'
+first_access_index_page_output=$(wget --retry-connrefused --no-check-certificate -T 30 http://localhost:8001 -O-)
+#echo $first_access_index_page_output
+if [ "$(echo \"$first_access_index_page_output\" | grep '>Belenios</a>' | wc -l)" != "1" ]; then echo "[First page access] First page access does not show a single '>Belenios</a>' text, but it should" && exit 1; else echo "[First page access] First page access shows a single '>Belenios</a>' text, as expected"; fi
+
+
+echo '/var/log/ reading...'
+tail -f /var/log/faillog
