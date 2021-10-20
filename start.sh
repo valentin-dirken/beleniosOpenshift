cc721352c2bf1e29e13fb479da3e551ee648a572
Valentin Dirken <3280182+valentin-dirken@users.noreply.github.com>
2021-10-20 23:45:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+whoami
 echo "cd /home/belenios/install"
 cd /home/belenios/install
+
 pwd
+
 echo "eval $(opam env)"
 eval $(opam env) 
+
 echo "/home/belenios/install/demo/run-server.sh &"
 /home/belenios/install/demo/run-server.sh &
 
@@ -13,10 +16,10 @@ echo 'Server launching...'
 sleep 5
 
 echo 'Server testing ...'
-first_access_index_page_output=$(wget --retry-connrefused --no-check-certificate -T 30 http://localhost:8001 -O-)
-#echo $first_access_index_page_output
+first_access_index_page_output=$(wget --retry-connrefused --no-check-certificate -T 30 http://localhost:8080 -O-)
+
 if [ "$(echo \"$first_access_index_page_output\" | grep '>Belenios</a>' | wc -l)" != "1" ]; then echo "[First page access] First page access does not show a single '>Belenios</a>' text, but it should" && exit 1; else echo "[First page access] First page access shows a single '>Belenios</a>' text, as expected"; fi
 
-
+# il faut toujours garder un processus qui ne s'arrête jamais comme un tail -f.
 echo '/var/log/ reading...'
 tail -f /var/log/faillog
